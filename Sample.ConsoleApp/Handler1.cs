35c4f8ef406f2e074b603df6da2e9e925fbc3d79
Trevor Reeves <trevorreeves@gmail.com>
2017-05-25 13:39:05 +0100

    Added sample app to demonstrate app startup service registration problem

---
+++ Sample.ConsoleApp/Handler1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Rebus.Handlers;
+
+namespace Sample.ConsoleApp
+{
+    public class Handler1 : IHandleMessages<Message1>
+    {
+        public Task Handle(Message1 message)
+        {
+            Console.WriteLine($"Handler1 received : {message}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Sample.ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Rebus.Config;
+using Rebus.Routing.TypeBased;
+using Rebus.ServiceProvider;
+using Rebus.Transport.InMem;
+
+namespace Sample.ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Perform service registrations
+            var services = new ServiceCollection();
+            services.AutoRegisterHandlersFromAssemblyOf<Handler1>();
+            services.AddSingleton<Producer>();
+
+            // 2. Configure and initialise Rebus... it's now listening & handling messages!
+            var adapter = new NetCoreServiceCollectionContainerAdapter(services);
+            Configure.With(adapter)
+                .Logging(l => l.ColoredConsole())
+                .Transport(t => t.UseInMemoryTransport(new InMemNetwork(), "Messages"))
+                .Routing(r => r.TypeBased().MapAssemblyOf<Message1>("Messages"))
+                .Start();
+
+            // 3. Potentially add more service registrations for the application, some of which
+            //    could be required by handlers.
+
+            // 4. Application starting...
+            var provider = services.BuildServiceProvider();
+
+            // 5. Application started...
+            var producer = provider.GetRequiredService<Producer>();
+            producer.Produce();
+        }
+    }
+}
